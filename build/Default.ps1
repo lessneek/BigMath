ae9d568a5eff95eeca5f6493feeb0e1cf5e8dcc2
Alexander Logger <intagger@live.ru>
2014-11-11 23:38:24 +0300

    Moved solution directly to the /src dir.

--- build/Default.ps1
+++ build/Default.ps1
@@ -1,10 +1,12 @@
 ﻿Properties {
+    $solution_name = "BigMath"
     $config = 'Debug'
     $packageVersion = $null
     $build_dir = Split-Path $psake.build_script_file
     $code_dir = "$build_dir\..\src"
-    $solution_path = "$code_dir\BigMath\BigMath.sln"
-    $assembly_info_path = "$code_dir\BigMath\CommonAssemblyInfo.cs"
+    $solution_path = "$code_dir\$solution_name.sln"
+    $assembly_info_path = "$code_dir\CommonAssemblyInfo.cs"
+    $nuspec_path = "$code_dir\$solution_name.nuspec"
 }
 
 FormatTaskName (("-"*25) + "[{0}]" + ("-"*25))
@@ -18,14 +20,14 @@ Task ValidateConfig {
     Assert ( 'Debug','Release' -contains $config) -failureMessage "Invalid config: $config; Valid values are 'Debug' and 'Release'."
 }
 
-Task Build -depends ValidateConfig -description "Builds outdated artifacts." {	
-    Write-Host "Building BigMath.sln" -ForegroundColor Green
+Task Build -depends ValidateConfig -description "Builds outdated artifacts." {
+    Write-Host "Building soulution..." -ForegroundColor Green
     Exec { msbuild "$solution_path" /t:Build /p:Configuration=$config /v:quiet } 
 }
 
 Task Clean -depends ValidateConfig -description "Deletes all build artifacts." {
-    Write-Host "Cleaning BigMath.sln" -ForegroundColor Green
-    Exec { msbuild "$solution_path" /t:Clean /p:Configuration=$config /v:quiet } 
+    Write-Host "Cleaning solution..." -ForegroundColor Green
+    Exec { msbuild "$solution_path" /t:Clean /p:Configuration=$config /v:quiet }
 }
 
 Task Rebuild -depends Clean,Build -description "Rebuilds all artifacts from source."
@@ -38,7 +40,7 @@ Task Pack -depends Build -description "Packs to a NuGet package." {
         rd $packages_dir -rec -force | out-null
     }
     mkdir $packages_dir | out-null
-    
+
     if (!$packageVersion)
     {
         Write-Host "Package version is not set, hence fetch version from AssemblyVersion attribute from $assembly_info_path."
@@ -46,8 +48,8 @@ Task Pack -depends Build -description "Packs to a NuGet package." {
         $regex = [regex] 'AssemblyVersion\("(?<Version>[0-9]+(?:\.(?:[0-9]+|\*)){1,3})"\)'
         $packageVersion = $regex.Match($assembly_info_content).Groups['Version'].Value
     }
-    
+
     Write-Host "Package version: $packageVersion."
 
-    Exec { nuget pack "$code_dir\BigMath\BigMath.nuspec" -Properties "config=$config" -Symbols -Version "$packageVersion" -OutputDirectory "$packages_dir" }
+    Exec { nuget pack $nuspec_path -Properties "config=$config" -Symbols -Version "$packageVersion" -OutputDirectory "$packages_dir" }
 }
